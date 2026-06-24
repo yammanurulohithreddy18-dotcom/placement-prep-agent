590643b76e1462ce1f2513a8c229a9db411ad73d
LIKITH REDDY <likithreddy@Mac.Dlink>
2026-06-24 13:18:22 +0530

    Fix Sentry setup

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+import os
 from utils.langsmith_setup import *
 
 import streamlit as st
 from ui.research_page import show_research
 from ui.chat_page import show_chat
 from ui.my_companies_page import show_my_companies
-import os
 import sentry_sdk
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
     send_default_pii=True,
+    traces_sample_rate=1.0,
 )
-1 / 0
+
+sentry_sdk.capture_message("Sentry is connected")
 st.set_page_config(
     page_title="Placement Prep Agent",
     layout="wide"
